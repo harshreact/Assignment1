42866b73ceee913970d91b65a069564240185ba0
harshreact <harshreactjs.aorc@gmail.com>
2025-01-28 12:08:45 +0530

    final changes

--- src/Componet/Metrics_section/Metrics_section.jsx
+++ src/Componet/Metrics_section/Metrics_section.jsx
@@ -3,20 +3,20 @@ import "./Metrics_section.css";
 const Metrics_section = () => {
   return (
     <section className='metrics_section'>
-        <div className='container'>
+        <div className='metrics_section_container'>
             <div className='metrics_section_main'>
                 <div className='metrics_section_certificate'>
                     <div className="left-panel"></div>
                     <div className="certificate_content">
-                        <h3>Key Insights From<br/>The National Customer<br/> Rage Survey</h3>
+                        <h4>Key Insights From<br/>The National Customer<br/> Rage Survey</h4>
                         <p className="subtitle">A Longitudinal Survey Of Customer Sentiments<br/>Regarding Complaint-handling In The United States</p>
+                        <div className="bottom-panel"></div>
+                        <p className="certificate_description">
+                            Conducted By Customer Care Measurement & Consulting (CCMC).<br/><br/>
+                            In Collaboration With The Center for Services Leadership (CSL) in the W. P. Carey School of Business<br/>
+                            at Arizona State University. January 2023
+                        </p>
                     </div>
-                    <div className="bottom-panel"></div>
-                    <p className="certificate_description">
-                        Conducted By Customer Care Measurement & Consulting (CCMC).<br/><br/>
-                        In Collaboration With The Center for Services Leadership (CSL) in the W. P. Carey School of Business<br/>
-                        at Arizona State University. January 2023
-                    </p>
                 </div>
                 <div className="metrics_section_right">
                     <p>READ THE LATEST NATIONAL CUSTOMER RAGE SURVEY FROM OUR PARTNER: CCMC</p>
